805b1b560602f3ac328dfcdfaa92917c6e7552bf
Jakub Ziemba <jayziembka@gmail.com>
2021-03-22 16:28:09 +0100

    Create Work page

---
+++ src/pages/work.js
@@ -0,0 +1,14 @@
+import { Title, Wrapper } from '../styles'
+
+import Card from '../components/Card'
+import React from 'react'
+import styled from 'styled-components'
+
+export default function Work() {
+  return (
+    <Wrapper>
+      <Title>Work</Title>
+      <Card>Roksana Machowska</Card>
+    </Wrapper>
+  )
+}
