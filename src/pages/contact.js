02c10b4c1239a3c6c01dc0497632491e1d525c3b
Jakub Ziemba <jayziembka@gmail.com>
2021-04-06 17:22:03 +0200

    Create contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Title, Wrapper } from '../styled'
+
+const Contact = () => {
+  return (
+    <Wrapper>
+      <Title>Contact</Title>
+    </Wrapper>
+  )
+}
+
+export default Contact
