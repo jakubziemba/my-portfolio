7cc2c9bdb2c0527785f6e8d6ce861eb8628725d6
Jakub Ziemba <jayziembka@gmail.com>
2021-04-06 17:21:16 +0200

    Create projects data file

---
+++ src/data/index.js
@@ -0,0 +1,28 @@
+const projects = [
+  {
+    id: 0,
+    path: 'forest-planting',
+    title: 'Forest Planting',
+    date: 'In Progress',
+    role: 'Developer',
+    tech: 'Gatsby, Styled Components, i18n, Framer Motion',
+  },
+  {
+    id: 1,
+    path: 'jagodajnia',
+    title: 'Jagodajnia',
+    date: '2021',
+    role: 'Designer / Developer',
+    tech: 'Gatsby, Styled Components, Framer Motion, Prismic',
+  },
+  {
+    id: 2,
+    path: 'roksana-machowska',
+    title: 'Roksana Machowska',
+    date: '2020',
+    role: 'Designer / Developer',
+    tech: 'Next.js, CSS Modules, Framer Motion',
+  },
+]
+
+export default projects
