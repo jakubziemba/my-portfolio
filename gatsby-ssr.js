f094aec9333cd4873e7242b1f30ffdcb8c20fe6e
Jakub Ziemba <jayziembka@gmail.com>
2021-04-07 22:40:46 +0200

    Fix ThemeProvider bug

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { GlobalStyle } from './src/theme/global-style'
+import { wrapRootElement as wrap } from './root-wrapper'
+
+export const wrapRootElement = wrap
 
 export const wrapPageElement = ({ element }) => (
   <>
