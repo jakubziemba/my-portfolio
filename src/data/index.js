cb99ffac21e9a6d3e2699c6f4723cd27516881ee
Jakub Ziemba <jayziembka@gmail.com>
2021-04-08 14:28:44 +0200

    Fix typo

--- src/data/index.js
+++ src/data/index.js
@@ -36,7 +36,7 @@ export const forestText = [
 ]
 
 export const jagodajniaText = [
-  'A website for a blueberry farm.',
+  'A website for a berry farm.',
   'Jagodajnia is a Haskap berry farm near Nysa, Poland. The client asked for a website with information about the farm, articles, gallery and contact form.',
   'After getting the content from the client, I created a simple website with a bit of animation to make it look alive and playful.',
   'For this project I used React (Gatsby) with Styled Components and Framer Motion. FlexyForm takes care of the backend of the contact form. The website is also connected to Prismic CMS for easier content management.',
