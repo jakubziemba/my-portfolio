5ee8e9e0376757d434016bfd575476e909c32fbb
Jakub Ziemba <jayziembka@gmail.com>
2021-04-08 13:54:47 +0200

    Add one-liner to project description

--- src/data/index.js
+++ src/data/index.js
@@ -26,20 +26,21 @@ const projects = [
 ]
 
 export const forestText = [
+  'A website for a board game.',
   'Forest Planting is a beautifully designed simulation board game and discussion experience designed by experienced leaders to help teams form, grow, multiply and collaborate.',
   "My role in the project as a developer was to bring the designer's ideas and solutions to life and to further develop the already existing parts of the website.",
   'For this project we are using React (Gatsby) with Styled Components, i18n for language variants (coming in the future) and Framer Motion for animations. Ultimately, Forest Planting website will be connected to a CMS for easier content management.',
 ]
 
 export const jagodajniaText = [
-  'A website for a blueberry farm',
+  'A website for a blueberry farm.',
   'Jagodajnia is a Haskap berry farm near Nysa, Poland. The client asked for a website with information about the farm, articles, gallery and contact form.',
   'After getting the content from the client, I created a simple website with a bit of animation to make it look alive and playful.',
   'For this project I used React (Gatsby) with Styled Components and Framer Motion. FlexyForm takes care of the backend of the contact form. The website is also connected to Prismic CMS for easier content management.',
 ]
 
 export const machowskaText = [
-  'A portfolio website for a polish artist',
+  'A portfolio website for a polish artist.',
   "For an artist, the most important quality of a website is that it will showcase and truly compliment their art. After seeing the client's work, consulting the ideas and inspirations, I designed and developed the portfolio website.",
   "Because of the inherent minimalism and abstract nature of Roksana's works, we've decided to keep the design as minimal as possible, letting the images, typography and animations play the main role.",
   'For the developement, I used React (Next.js) with CSS Modules and Framer Motion for the animations.',
