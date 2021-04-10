df5b265783fc8f664c6c0d86da30e4cf7251fc40
Jakub Ziemba <jayziembka@gmail.com>
2021-04-10 02:37:12 +0200

    Fix the glitch after clicking links

--- src/components/FadeIn.js
+++ src/components/FadeIn.js
@@ -5,13 +5,18 @@ const FadeIn = ({ children }) => {
   const ref = useRef()
 
   useEffect(() => {
+    gsap.to([ref.current], 0, { css: { visibility: 'visible' } })
     gsap.fromTo(
       [ref.current],
       { opacity: 0, y: 30 },
       { opacity: 1, y: 0, duration: 0.7, delay: 1 }
     )
   }, [])
-  return <div ref={ref}>{children}</div>
+  return (
+    <div ref={ref} style={{ visibility: 'hidden' }}>
+      {children}
+    </div>
+  )
 }
 
 export default FadeIn
