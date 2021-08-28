7776c764881f2246727483f604322ff37177f8a6
Jakub Ziemba <ziemba.jak@gmail.com>
2021-08-28 12:25:25 +0200

    fix/delete Jagodajnia project

--- src/data/index.js
+++ src/data/index.js
@@ -10,21 +10,17 @@ const projects = [
   },
   {
     id: 1,
-    path: 'jagodajnia',
-    title: 'Jagodajnia',
-    date: '2021',
-    role: 'Design / Development',
-    tech: 'Gatsby, Styled Components, Framer Motion, FlexyForm, Prismic',
-    category: 'Website',
-  },
-  {
-    id: 2,
     path: 'roksana-machowska',
     title: 'Roksana Machowska',
     date: '2020',
     role: 'Design / Developement',
     tech: 'Next.js, CSS Modules, Framer Motion',
     category: 'Website',
+  },
+  {
+    id: 3,
+    title: 'More projects coming soon...',
+    date: 2021,
   },
 ]
 
@@ -33,13 +29,6 @@ export const forestText = [
   'Forest Planting is a beautifully designed simulation board game and discussion experience designed by experienced leaders to help teams form, grow, multiply and collaborate.',
   "My role in the project as a developer was to bring the designer's ideas and solutions to life and to further develop the already existing parts of the website.",
   'For this project we are using React (Gatsby) with Styled Components, i18n for language variants (coming in the future) and Framer Motion for animations. Ultimately, Forest Planting website will be connected to a CMS for easier content management.',
-]
-
-export const jagodajniaText = [
-  'A website for a berry farm.',
-  'Jagodajnia is a Haskap berry farm near Nysa, Poland. The client asked for a website with information about the farm, articles, gallery and contact form.',
-  'After getting the content from the client, I created a simple website with a bit of animation to make it look alive and playful.',
-  'For this project I used React (Gatsby) with Styled Components and Framer Motion. FlexyForm takes care of the backend of the contact form. The website is also connected to Prismic CMS for easier content management.',
 ]
 
 export const machowskaText = [
