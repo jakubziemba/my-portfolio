90e90cc00d9f456070e6ed030619b3d297d0a1b5
Jakub Ziemba <ziemba.jak@gmail.com>
2022-04-15 01:00:46 +0200

    fix/fix texts

--- src/data/index.js
+++ src/data/index.js
@@ -28,13 +28,14 @@ const projects = [
 
 export const haitText = [
   'A meme service for Nepali people.',
-  'Hait is a project started by my friend and previous coding mentor Marcin, who asked me to create a design for a Nepali meme service, which his coding students from Nepali orphanage will implement in React. The challenge was to create a simple design that would be easy to use and understand, but also not too complicated for beginner developers to implement in code.',
-  "My role in the project was to conduct interviews, in pair with Julian - my friend and UX Designer, with Nepali people, do a desk research about Nepali culture and internet usage, create mockups and UI. The timeline wasn't long - we had 2 months to finish the project. We were asked to use Material UI elements, which wasn't my favorite choice because of creative constraints, but I tried to use it to the best of my possibilites.",
+  'Hait is a project started by my friend and previous coding mentor Marcin, who asked me to create a design for a meme service, which his coding students from Nepali orphanage will implement in React. The challenge was to create a simple design that would be both easy to use and understand, but also not too complicated for beginner developers to implement in code.',
+  "My role in the project was to conduct interviews - in pair with Julian - my friend and UX Designer, with Nepali people. Besides that I've done a desk research about their culture and internet usage, created mockups and UI. We had 2 months to finish the project and managed well to keep the deadlines.",
+  "We were asked to use Material UI elements, which wasn't my favorite choice because of creative constraints, but I tried to use it to the best of my possibilites.",
   'The website is currently in development and is expected to be released by the end of April 2022.',
 ]
 
 export const machowskaText = [
-  'A portfolio website for a polish artist.',
+  'A portfolio website for a Polish artist.',
   "For an artist, the most important quality of a website is that it will showcase and truly compliment their art. After seeing the client's work, consulting the ideas and inspirations, I designed and developed the portfolio website.",
   "Because of the inherent minimalism and abstract nature of Roksana's works, we've decided to keep the design as minimal as possible, letting the images, typography and animations play the main role.",
   'For the developement, I used React (Next.js) with CSS Modules and Framer Motion for the animations.',
